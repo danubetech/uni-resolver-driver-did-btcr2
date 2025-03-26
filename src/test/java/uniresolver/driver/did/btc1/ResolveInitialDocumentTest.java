4d35f19fb779d335edcea0b6238fd160adeaf125
Markus Sabadello <markus@danubetech.com>
2025-03-26 15:17:40 +0100

    chore: Update test vector to match https://github.com/dcdpr/did-btc1/tree/notebooks-with-local-btc-node/TestVectors

--- src/test/java/uniresolver/driver/did/btc1/ResolveInitialDocumentTest.java
+++ src/test/java/uniresolver/driver/did/btc1/ResolveInitialDocumentTest.java
@@ -1,7 +1,9 @@
 package uniresolver.driver.did.btc1;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import foundation.identity.did.DID;
 import foundation.identity.did.DIDDocument;
+import org.erdtman.jcs.JsonCanonicalizer;
 import org.junit.jupiter.api.Test;
 import uniresolver.driver.did.btc1.crud.read.ParseDidBtc1Identifier;
 import uniresolver.driver.did.btc1.crud.read.ResolveInitialDocument;
@@ -14,6 +16,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ResolveInitialDocumentTest {
 
+	private static final ObjectMapper objectMapper = new ObjectMapper();
+
 	@Test
 	public void testResolve() throws Exception {
 
@@ -22,10 +26,16 @@ public class ResolveInitialDocumentTest {
 		DID identifier = DID.fromString(TestUtil.readResourceString("did.txt"));
 		IdentifierComponents identifierComponents = ParseDidBtc1Identifier.parseDidBtc1Identifier(identifier);
 		Map<String, Object> resolutionOptions = new HashMap<>();
+
 		DIDDocument initialDIDDocument = resolveInitialDocument.resolveInitialDIDDocument(identifier, identifierComponents, resolutionOptions);
+		String initialDIDDocumentCanonicalized = new JsonCanonicalizer(initialDIDDocument.toJson()).getEncodedString();
+		Map<String, Object> initialDIDDocumentMap = (Map<String, Object>) objectMapper.readValue(initialDIDDocumentCanonicalized, Map.class);
 
 		DIDDocument expectedInitialDIDDocument = DIDDocument.fromMap(TestUtil.readResourceJson("initialDidDocument.json"));
+		String expectedInitialDIDDocumentCanonicalized = new JsonCanonicalizer(expectedInitialDIDDocument.toJson()).getEncodedString();
+		Map<String, Object> expectedInitialDIDDocumentMap = (Map<String, Object>) objectMapper.readValue(expectedInitialDIDDocumentCanonicalized, Map.class);
 
-		assertEquals(expectedInitialDIDDocument, initialDIDDocument);
+		assertEquals(expectedInitialDIDDocumentCanonicalized, initialDIDDocumentCanonicalized);
+		assertEquals(expectedInitialDIDDocumentMap, initialDIDDocumentMap);
 	}
 }
