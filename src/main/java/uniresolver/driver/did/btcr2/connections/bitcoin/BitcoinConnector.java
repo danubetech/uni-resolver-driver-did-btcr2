4c7b063056a8567430ae5732625047a74af1f8b7
Markus Sabadello <markus@danubetech.com>
2025-10-12 15:19:37 +0200

    Remove default genesis hash for "signet"

--- src/main/java/uniresolver/driver/did/btcr2/connections/bitcoin/BitcoinConnector.java
+++ src/main/java/uniresolver/driver/did/btcr2/connections/bitcoin/BitcoinConnector.java
@@ -33,7 +33,6 @@ public class BitcoinConnector {
 						Network.testnet4, BitcoindRPCBitcoinConnection.create(BitcoinJSONRPCClient.DEFAULT_JSONRPC_TESTNET_URL)),
 				Map.of(
 						Network.bitcoin, "6fe28c0ab6f1b372c1a6a246ae63f74f931e8365e15a089c68d6190000000000",
-						Network.signet, "f61eee3b63a380a477a063af32b2bbc97c9ff9f01f2c4225e973988108000000",
 						Network.regtest, "06226e46111a0b59caaf126043eb5bbf28c34f3a5e332a1fc7b2b73cf188910f",
 						Network.testnet3, "43497fd7f826957108f4a30fd9cec3aeba79972084e90ead01ea330900000000",
 						Network.testnet4, "43f08bdab050e35b567c864b91f47f50ae725ae2de53bcfbbaf284da00000000")
