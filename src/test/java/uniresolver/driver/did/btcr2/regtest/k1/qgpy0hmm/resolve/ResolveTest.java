fde91a0991e63f641a004d57bd4414899e569bb2
Markus Sabadello <markus@danubetech.com>
2026-03-31 19:13:57 +0200

    Disable Polar regtest test vectors

--- src/test/java/uniresolver/driver/did/btcr2/regtest/k1/qgpy0hmm/resolve/ResolveTest.java
+++ src/test/java/uniresolver/driver/did/btcr2/regtest/k1/qgpy0hmm/resolve/ResolveTest.java
@@ -1,7 +1,9 @@
 package uniresolver.driver.did.btcr2.regtest.k1.qgpy0hmm.resolve;
 
+import org.junit.jupiter.api.Disabled;
 import uniresolver.driver.did.btcr2.AbstractResolveTest;
 
+@Disabled("Requires local Polar instance")
 public class ResolveTest extends AbstractResolveTest {
 
     public ResolveTest() {
--- src/test/java/uniresolver/driver/did/btcr2/regtest/x1/qfl7se8f/resolve/ResolveTest.java
+++ src/test/java/uniresolver/driver/did/btcr2/regtest/x1/qfl7se8f/resolve/ResolveTest.java
@@ -1,7 +1,9 @@
 package uniresolver.driver.did.btcr2.regtest.x1.qfl7se8f.resolve;
 
+import org.junit.jupiter.api.Disabled;
 import uniresolver.driver.did.btcr2.AbstractResolveTest;
 
+@Disabled("Requires local Polar instance")
 public class ResolveTest extends AbstractResolveTest {
 
 	public ResolveTest() {
