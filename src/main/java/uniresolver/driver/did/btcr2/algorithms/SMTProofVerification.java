e2baef02d29c49f801173d1863153b4fc3b82eae
Markus Sabadello <markus@danubetech.com>
2026-05-08 09:16:27 +0200

    Fix comment

--- src/main/java/uniresolver/driver/did/btcr2/algorithms/SMTProofVerification.java
+++ src/main/java/uniresolver/driver/did/btcr2/algorithms/SMTProofVerification.java
@@ -3,7 +3,7 @@ package uniresolver.driver.did.btcr2.algorithms;
 import uniresolver.driver.did.btcr2.data.json.SMTProof;
 
 /*
- * JSON Document Hashing
+ * SMT Proof Verification
  * See https://dcdpr.github.io/did-btcr2/algorithms.html#smt-proof-verification
  */
 public class SMTProofVerification {
