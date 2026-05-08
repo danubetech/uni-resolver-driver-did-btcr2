d11b2a30ff004f064cefcc457e64565b0778dad2
Markus Sabadello <markus@danubetech.com>
2026-05-08 09:58:46 +0200

    Add constructor

--- src/main/java/uniresolver/driver/did/btcr2/data/json/CASAnnouncement.java
+++ src/main/java/uniresolver/driver/did/btcr2/data/json/CASAnnouncement.java
@@ -1,7 +1,15 @@
 package uniresolver.driver.did.btcr2.data.json;
 
 import java.util.LinkedHashMap;
+import java.util.Map;
 
 public class CASAnnouncement extends LinkedHashMap<String, String> {
 
+    public CASAnnouncement() {
+        super();
+    }
+
+    public CASAnnouncement(Map<? extends String, ? extends String> m) {
+        super(m);
+    }
 }
